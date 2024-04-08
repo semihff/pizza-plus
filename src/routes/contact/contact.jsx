9c643206bc65798dcc97244a15803d9a6030188f
Semih Basyigit <semihbasyigit@proton.me>
2024-04-08 17:27:37 +0300

    home components created

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <iframe
+          width="40%"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Merdivenk%C3%B6y,%20H%C4%B1z%C4%B1rbey%20Cd.%20No:160/C,%2034732%20Kad%C4%B1k%C3%B6y/%C4%B0stanbul+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.gps.ie/">gps tracker sport</a>
+        </iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Navbar from "../../components/navbar";
+import "./home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <main>
+        <section>
+          <h1>Pizza Plus</h1>
+          <p>Bir Yayıncının Elinden...</p>
+          <span>Muhammed Demirel</span>
+        </section>
+        <aside>
+          <img className="pizza" src="/src/images/pizza.png" alt="" />
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
