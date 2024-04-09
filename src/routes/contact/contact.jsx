43b978f2b0066577fbab581c137264229ea27a47
Semih Basyigit <semihbasyigit@proton.me>
2024-04-09 18:46:43 +0300

    project finished

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,20 +1,57 @@
 import React from "react";
-
+import Navbar from "../../components/navbar";
+import { PiArrowBendRightDownLight } from "react-icons/pi";
+import { FiPhone } from "react-icons/fi";
+import "./contact.css";
 const Contact = () => {
   return (
     <div>
-      <div>
-        <iframe
-          width="40%"
-          height="600"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Merdivenk%C3%B6y,%20H%C4%B1z%C4%B1rbey%20Cd.%20No:160/C,%2034732%20Kad%C4%B1k%C3%B6y/%C4%B0stanbul+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-        >
-          <a href="https://www.gps.ie/">gps tracker sport</a>
-        </iframe>
+      <Navbar />
+      <div className="container">
+        <div className="info">
+          <div className="order">
+            <div className="order-name">
+              <h1>Sipariş</h1>
+              <PiArrowBendRightDownLight
+                className="arrow"
+                size={50}
+                color="#FF8C42"
+              />
+            </div>
+            <a
+              target="_blank"
+              href="https://www.yemeksepeti.com/restaurant/owg6/pizza-owg6?utm_source=google&utm_medium=organic&utm_campaign=google_reserve_place_order_action"
+            >
+              <button>Yemeksepeti</button>
+            </a>
+          </div>
+          <div className="contact">
+            <div className="phone">
+              <FiPhone size={50} color="#FF8C42" />
+              <span>0551 888 1114</span>
+            </div>
+            <p>
+              Merdivenköy, Hızırbey Cd. <br /> No:160/C, 34732 Kadıköy/İstanbul
+            </p>
+          </div>
+        </div>
+        <div className="map">
+          <h1>Konumumuz</h1>
+          <div className="map-wrapper">
+            <img className="slice" src="/public/images/slice.png" alt="" />
+            <div className="google-map">
+              <iframe
+                width="100%"
+                height="600"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Merdivenk%C3%B6y,%20H%C4%B1z%C4%B1rbey%20Cd.%20No:160/C,%2034732%20Kad%C4%B1k%C3%B6y/%C4%B0stanbul+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              ></iframe>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/routes/menu/menu.jsx
+++ src/routes/menu/menu.jsx
@@ -1,87 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar";
 import "./menu.css";
+import { products } from "../../../constants";
 
-const products = [
-  {
+const Menu = () => {
+  const [selectedPizza, SetSelectedPizza] = useState({
     id: 1,
     name: "Salsiccia Pizza",
     prize: "289",
     contents:
       "Mozzarella peyniri, domates sosu, sucuk, mısır, kırmızıbiber, yeşil biber",
-    img: "/src/images/salsiccia.png",
-  },
-  {
-    id: 2,
-    name: "Margherita Pizza",
-    prize: "195",
-    contents:
-      "Mozzarella peyniri, domates sosu, çeri domates, parmesan peyniri, fesleğen",
-    img: "/src/images/margherita.png",
-  },
-  {
-    id: 3,
-    name: "Formaggi Pizza",
-    prize: "318",
-    contents:
-      "Mozzarella peyniri, beyaz sos, taze mozzarella, cheddar peyniri, beyaz peynir, parmesan peyniri, fesleğen",
-    img: "/src/images/formaggi.png",
-  },
-  {
-    id: 4,
-    name: "Al Funghi Pizza",
-    prize: "242",
-    contents: "Mozzarella peyniri, domates sosu, mantar, sarımsaklı zeytinyağı",
-    img: "/src/images/alfunghi.png",
-  },
-  {
-    id: 5,
-    name: "Mediterranea Pizza",
-    prize: "263",
-    contents: "Mozzarella peyniri, domates sosu, siyah zeytin, fesleğen, kekik",
-    img: "/src/images/mediterranea.png",
-  },
-  {
-    id: 6,
-    name: "Tonna Cipolla Pizza",
-    prize: "195",
-    contents: "Mozzarella peyniri, domates sosu, ton balığı, mor soğan, mısır",
-    img: "/src/images/tonnacipo.png",
-  },
-  {
-    id: 7,
-    name: "Mixed Pizza",
-    prize: "318",
-    contents:
-      "Mozzarella peyniri, domates sosu, sucuk, jambon, siyah zeytin, mantar, mısır, yeşil biber, kırmızı biber, parmesan peyniri",
-    img: "/src/images/mixed.png",
-  },
-  {
-    id: 8,
-    name: "Bufala Pizza",
-    prize: "242",
-    contents:
-      "Mozzarella peyniri, domates sosu, taze mozzarella peyniri, parmesan peyniri, fesleğen",
-    img: "/src/images/bufala.png",
-  },
-  {
-    id: 9,
-    name: "Chicken BBQ Pizza",
-    prize: "263",
-    contents:
-      "Mozzarella peyniri, domates sosu, tavuk parçaları, mantar, barbekü sos",
-    img: "/src/images/chickenbbq.png",
-  },
-  {
-    id: 10,
-    name: "Vegetarian Pizza",
-    prize: "195",
-    contents:
-      "Mozzarella peyniri, domates sosu, yeşil biber, kırmızı biber, mantar, közlenmiş patlıcan, parmesan peyniri, sarımsaklı zeytinyağı",
-    img: "/src/images/vegetarian.png",
-  },
-];
-const Menu = () => {
+    img: "/public/images/salsiccia.png",
+  });
+  const changePizza = (id) => {
+    const selectedPizzaId = products.find((pizza) => pizza.id === id);
+    SetSelectedPizza(selectedPizzaId);
+  };
   return (
     <div className="menu-wrapper">
       <Navbar />
@@ -90,18 +24,31 @@ const Menu = () => {
           <h1>Menü</h1>
         </div>
         <main>
-          <img className="half-pizza" src="/src/images/halfpizza.png" alt="" />
           <div className="prize">
             <h1>Pizzalar</h1>
-            {products.map((item) => (
-              <div className="pizza" key={item.id}>
-                <span>{item.name}</span>
-                <span>....................</span>
-                <span>{item.prize}</span>
-              </div>
-            ))}
+            <div className="pizza-wrapper">
+              {products.map((item) => (
+                <div
+                  onClick={() => changePizza(item.id)}
+                  className={`pizza ${
+                    selectedPizza.id === item.id && "selected-pizza"
+                  } `}
+                  key={item.id}
+                >
+                  <span className="pizza-name">{item.name}</span>
+                  <div className="seprator" />
+                  <span className="pizza-prize">{item.prize} TL</span>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="pizza-details"></div>
+          <div className="pizza-details">
+            <h1>{selectedPizza.name}</h1>
+            <div className="selected-img">
+              <img src={selectedPizza.img} alt="" />
+            </div>
+            <p>{selectedPizza.contents}</p>
+          </div>
         </main>
       </div>
     </div>
